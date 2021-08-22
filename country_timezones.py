899054d27e4398d9880c53558d1e0a33dd24165b
janhavi <janhavi.n@protonmail.com>
2021-08-22 17:02:35 +0530

    initial commit - timezone display

---
+++ country_timezones.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+tkinter practice
+"""
+import tkinter
+import pytz
+import datetime
+
+gui_window = tkinter.Tk()
+gui_window.title("Time Zones")
+gui_window.geometry("640x480+20+300")
+
+
+
+gui_window.mainloop()
